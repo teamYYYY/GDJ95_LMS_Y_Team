3d8ee4c8eadce3a59bc752435e48e006d4a8c7d1
GD <GD@DESKTOP-66MHREQ>
2025-11-24 12:19:18 +0900

    dto 구현

---
+++ src/main/java/com/example/lms/dto/AttendanceDTO.java
@@ -0,0 +1,11 @@
+package com.example.lms.dto;
+
+public class AttendanceDTO {
+	private int attendanceINo;
+	private int studentUserNo;
+	private int courseNo;
+	private int lectureNo;
+	private int attendanceStatus;
+	private String createdate;
+	private String updatedate;
+}
